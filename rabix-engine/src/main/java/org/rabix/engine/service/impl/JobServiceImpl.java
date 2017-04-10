dfa745066ca167937ede654b3bb37c58a079cc82
Milos Ljubinkovic <milos.ljubinkovic@sbgenomics.com>
2017-04-10 19:01:04 +0200

    Quick revert for command line fail

--- rabix-engine/src/main/java/org/rabix/engine/service/impl/JobServiceImpl.java
+++ rabix-engine/src/main/java/org/rabix/engine/service/impl/JobServiceImpl.java
@@ -168,7 +168,7 @@ public class JobServiceImpl implements JobService {
           default:
             break;
           }
-          jobRepository.updateShort(job);
+          jobRepository.update(job);
           eventProcessor.persist(statusEvent);
           eventWrapper.set(statusEvent);
           isSuccessful.set(true);
